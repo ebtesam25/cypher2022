ae9e3311815abeec00ec54f968c4b77c574fd0c1
ebtesam25 <ehaque4@gmu.edu>
2022-03-25 21:40:41 -0400

    login screen

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,32 +2,34 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  themes: [
-    {
-      immigrace: {
-      
-"primary": "#1E698D",
-      
-"secondary": "#F0BB29",
-      
-"accent": "#1E698E",
-      
-"neutral": "#F0BB19",
-      
-"base-100": "#FFFFFF",
-      
-"info": "#3ABFF8",
-      
-"success": "#36D399",
-      
-"warning": "#FBBD23",
-      
-"error": "#F87272",
-      },
-    },
-  ],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+            "primary": "#1E698D",
+                    
+            "secondary": "#F0BB29",
+                    
+            "accent": "#1E698E",
+                    
+            "neutral": "#F0BB19",
+                    
+            "base-100": "#FFFFFF",
+                    
+            "info": "#3ABFF8",
+                    
+            "success": "#36D399",
+                    
+            "warning": "#FBBD23",
+                    
+            "error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
