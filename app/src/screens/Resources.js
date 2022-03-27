09282c16c9d5a85f89519d56ceb283f8610793d4
ebtesam25 <ehaque4@gmu.edu>
2022-03-26 23:19:49 -0400

    search engine integrated

--- app/src/screens/Resources.js
+++ app/src/screens/Resources.js
@@ -4,6 +4,7 @@ import data from '../utils/data.json'
 
 export default function Resources() {
     const [id, setid] = useState(0)
+    const [search, setsearch] = useState('')
   return (
     <div className="App">
       <Navbar/>
@@ -25,6 +26,14 @@ export default function Resources() {
         </label>
         </label>
     <body>
+    <div class="form-control p-4 w-screen">
+  <div class="input-group">
+    <input type="text" placeholder="Search…" value={search} onChange={(e)=>setsearch(e.target.value)} class="input input-bordered"/>
+    <button class="btn btn-square">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+</div>
       <div class="overflow-x-auto">
   <table class="table w-full">
     <thead className="bg-secondary">
@@ -36,7 +45,7 @@ export default function Resources() {
       </tr>
     </thead>
     <tbody>
-      {data.map((item)=>(<tr>
+      {data.map((item)=>((item.tags.includes(search) || item.description.includes(search) || item.category.includes(search) || item['name of resource'].includes(search))&&<tr>
         <th>{item.id+1}</th>
         <td><div onClick={()=>setid(parseInt(item.id))}>
             <label for="details" class="btn-ghost">{item['name of resource']}</label>
